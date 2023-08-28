2e90ab82ff5152366f55510b857e8295f41fbf29
DebonairDespot <robotedan123@gmail.com>
2023-08-28 12:14:19 +0700

    Basic Movemet + Wall Jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (wallJumpCooldown > 0.2f)
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                rb.gravityScale = 0;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                rb.gravityScale = 1.5f;
+            }
+
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+    }
+
+    void Jump()
+    {
+        if(isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        } 
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                rb.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+
+            wallJumpCooldown = 0;
+        } 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+}
